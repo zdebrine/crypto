e2ea98608f59c493fbbec17f5a1b0820f51fc4d7
Zak DeBrine <zdebrine@gmail.com>
2020-11-05 10:55:03 -0700

    Configured app with webpack, Rendering to dom

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.static('../client/dist')); // Host your dist folder up to the server
+app.use(express.json()); // Alternative to BodyParser
+
+// If you had to handle requests on the server side, this is where that would occur
+app.get('/products/:id', (req, res) => {
+		// Handle the request
+		// -- Could make DB queries here
+		// Send back O-K
+		res.status(200).send('The server is taking requests to the products/:id endpoint');
+});
+
+// Listening for requests on the PORT
+app.listen(PORT, () => {
+    console.log('Serving up now at '+ JSON.stringify(PORT))
+});
