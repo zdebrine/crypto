a38a7e8722f6fb75775707ff6b48e5410b67c6f8
Zak DeBrine <zdebrine@gmail.com>
2020-11-05 11:18:44 -0700

    Set up initial layout with optional light mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
 
 const App = () => {
-    return ( 
-        <Grid container direction="row">
-            <Grid item xs={12} md={6}>
-                Hello Cryps?
-            </Grid>
+  return (
+    <body>
+      <Grid container direction="row">
+        <Grid item xs={12} md={6}>
+          Hello Cryps?
         </Grid>
-    );
-}
+      </Grid>
+    </body>
+  );
+};
 
 export default App;
