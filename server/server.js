addfb9cba4c5f56baac564e2595c8665205170f3
Zak DeBrine <zdebrine@gmail.com>
2020-11-05 12:45:07 -0700

    Added request handler for getting currency data

--- server/server.js
+++ server/server.js
@@ -1,22 +1,26 @@
-const express = require('express');
+require('dotenv').config();
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const PORT = 3000;
+const TOKEN = process.env.TOKEN;
+console.log(TOKEN);
 
 app.use(cors());
-app.use(express.static('../client/dist')); // Host your dist folder up to the server
+app.use(express.static("../client/dist")); // Host your dist folder up to the server
 app.use(express.json()); // Alternative to BodyParser
 
 // If you had to handle requests on the server side, this is where that would occur
-app.get('/products/:id', (req, res) => {
-		// Handle the request
-		// -- Could make DB queries here
-		// Send back O-K
-		res.status(200).send('The server is taking requests to the products/:id endpoint');
+app.get("/prices/:currency", (req, res) => {
+  axios.get(
+    `https://api.nomics.com/v1/currencies/sparkline?key=${TOKEN}&ids=${req.params.currency}&start=2018-04-14T00%3A00%3A00Z&end=2018-05-14T00%3A00%3A00Z`)
+    .then((data) => res.send(data))
+    .catch((err) => console.error(err));
 });
 
 // Listening for requests on the PORT
 app.listen(PORT, () => {
-    console.log('Serving up now at '+ JSON.stringify(PORT))
+  console.log("Serving up now at " + JSON.stringify(PORT));
 });
