7fcbab5db7b76d964ba75195bd1ef5c86b2764e2
Zak DeBrine <zdebrine@gmail.com>
2020-11-05 11:11:18 -0700

    Added background colors and set up basic styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
 const App = () => {
     return ( 
-        <div>
-            Hello World!
-        </div>
+        <Grid container direction="row">
+            <Grid item xs={12} md={6}>
+                Hello Cryps?
+            </Grid>
+        </Grid>
     );
 }
 
